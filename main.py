9e590f27df510d0308bfa02ded2d59bdd1325923
Michael-Owen-19 <68501676+Michael-Owen-19@users.noreply.github.com>
2020-11-14 02:38:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,6 @@ for i in range (panjang(teks)):
 
 Query=jadiinvektor(Q,term)
 
-#print(M)
-
-#for i in range (panjang(M)):
-#    print(similarity(M[i],Query))
-
-#Kumpulan array teksnya supaya nanti bisa dikeluarin
-
 #Definisiin kamusnya, supaya vektor itu selalu berpasangan sama teks
 dict=dictionary(K,M,Query)
 judul = title(K, judulfile)
@@ -45,3 +38,28 @@ for i in range (panjang(M)):
     for key in K:
         if(dict[key]==M[i]):
             z.append(key)
+
+#menampilkan tabel term
+base=basis(Q)
+judultabel=["Term","Query","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","D11","D12","D13","D14","D15"]
+termTable=[[0 for i in range(panjang(base)+1)]for j in range(panjang(judultabel))]
+termTable[0]=judultabel
+for i in range(panjang(base)):
+    termTable[i+1][0]=base[i]
+NewQuery=jadiinvektor(Q,base)
+for i in range(panjang(base)):
+    termTable[i+1][1]=NewQuery[i]
+NewM=[]
+for i in range (panjang(teks)):
+    NewM.append(jadiinvektor(teks[i],base))
+for i in range(panjang(base)+1):
+    for j in range(panjang(judultabel)):
+        if i>0 & j>1:
+            termTable[i][j]=NewM[j-2][i-1]
+#for i in range(panjang(TermTable))
+#for i in range(panjang(judultabel)):
+#    for j in range (panjang(base)+1):
+#        print(termTable[i][j])
+#        print(" ")
+#    print("")
+print(termTable)
