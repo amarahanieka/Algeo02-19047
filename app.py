ff958f92b62b170e24d1e0a5f5cecec2cdb5e4de
amarahanieka <68474184+amarahanieka@users.noreply.github.com>
2020-11-14 01:54:06 +0700

    upload wowww

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from functions import *
+from werkzeug.utils import secure_filename
+import os
 
 # App config.
 DEBUG = True
@@ -9,10 +11,53 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+# Get current path
+path = os.getcwd()
+# file Upload
+UPLOAD_FOLDER = os.path.join(path, 'filetxt')
+
+# Make directory if uploads is not exists
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Allowed extension you can set your own
+ALLOWED_EXTENSIONS = set(['txt'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        flash('File(s) successfully uploaded')
+        return redirect('/search')
+
+
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
 
-    @app.route("/", methods=['GET', 'POST'])
+    @app.route("/search", methods=['GET', 'POST'])
     def hello():
         form = ReusableForm(request.form)
         print(form.errors)
@@ -38,7 +83,7 @@ class ReusableForm(Form):
             # Buat nampilin judul
 
             titleteks1 = "Naik ke puncak gunung"
-            titleteks2 = "Saingan Trump"
+            titleteks2 = "Partai Demokrat"
             titleteks3 = "Ayah ibu"
 
             titleteks = [titleteks1, titleteks2, titleteks3]
@@ -99,7 +144,7 @@ class ReusableForm(Form):
             else:
                 return render_template('notfound.html', form=form)
         else:
-            flash('Error: All the form fields are required. ')
+            flash('Anda tidak memasukkan query apapun.')
             return render_template('hello.html', form=form)
 
 @app.route('/nyoba/')
